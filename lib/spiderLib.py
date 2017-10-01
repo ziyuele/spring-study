cd204cf08e22facdaa1c2b510f8c74240e354799
kangjian03 <2429362606@qq.com>
2017-10-02 00:29:11 +0800

    add spider lib case

--- lib/spiderLib.py
+++ lib/spiderLib.py
@@ -41,7 +41,7 @@ class spiderLib():
         res = response.read()
         return  res
 
-    def doSpiderGet(self, url, args_dict=""):
+    def doSpiderGet(self, url, args_dict=None):
         """
 
         :param url:do spider with requet https
@@ -49,10 +49,75 @@ class spiderLib():
         :return:
         """
         url = "https://" + url
-        if args_dict != "" :
+        if args_dict != None :
             requestData = urllib.urlencode(args_dict)
             url = url + "?" + requestData
         request = urllib2.Request(url)
         response = urllib2.urlopen(request)
-        return response
+        return response.read()
 
+    def doSpiderPost(self, url, args_dict=None):
+        """
+        :param url: the url spider for
+        :param args_dict: input post data
+        :return: spider results
+        """
+        url = "https://" + url
+        if args_dict != None:
+            requestData = urllib.urlencode(args_dict)
+            request = urllib2.Request(url,requestData)
+            response = urllib2.urlopen(request)
+        else:
+            request = urllib2.Request(url)
+            response = urllib2.urlopen(url)
+        return response.read()
+
+    def doSpiderHeaders(self, url, args_dict=None):
+        """
+        do spider useing setting headers
+        :param url: the url spider for
+        :param args_dict: args_dict = { "data" : "xxx",
+                                        "headers" :{ "User_agent": "xxx"}
+                                    }
+        :return: query result
+        """
+        if args_dict is None:
+            args_dict = {"values": "", \
+                         "headers": ""}
+        url = "https://" + url
+        data  = urllib.urlencode(args_dict["data"])
+        headers = args_dict["headers"]
+        request = urllib2.Request(url, data, headers)
+        response = urllib2.urlopen(request)
+        return response.read()
+
+    def doSpiderProxy(self, url, args_dict=None ,proxy_enable=False):
+        """
+        used to do spider using proxy
+        :param url: the url spider for
+        :param args_dict: args_dict ={
+            "headers":{"User_agent": "",
+                       "***": "***"},
+            "data":{"***": "***",
+                    "***": "***",
+                    ......}
+            proxySetting : {"***": "*****"}
+        }
+        :return: spider results
+        """
+        if args_dict == None:
+            return 1
+        else:
+            try:
+                if proxy_enable == True:
+                    proxy_header = urllib2.ProxyHandler(args_dict["proxySetting"])
+                else:
+                    proxy_header = urllib2.ProxyHandler({})
+
+                opener = urllib2.build_opener(proxy_header)
+                urllib2.install_opener(opener)
+                res = self.doSpiderHeaders(url, args_dict)
+                return res
+            except Exception, e:
+                print "argument error",e
+                return -1
--- tests/testSpider.py
+++ tests/testSpider.py
@@ -38,7 +38,7 @@ class MyTestCase(unittest.TestCase):
         """
         url = "www.baidu.com"
         res = self.spider.doSpider(url)
-        if "!--STATUS OK--" in res:
+        if "html" in res:
             flag = True
         else:
             flag = False
@@ -55,7 +55,71 @@ class MyTestCase(unittest.TestCase):
              "password" : "******"
         }
         res = self.spider.doSpiderGet(url, args_dict)
+        if "html" in res:
+            flag = True
+        else:
+            flag = False
+        self.assertEqual(flag, True, "do spider test fail")
+        res = self.spider.doSpiderGet(url)
+        if "html" in res:
+            flag = True
+        else:
+            flag = False
+        self.assertEqual(flag, True, "do spider tet fail")
+
+    def test_doSpiderPost(self):
+        """
+        this UT used to test POST method for sipder
+        :return:
+        """
+        url = "passport.csdn.net/account/login"
+        args_dict = {
+            "userName":"2429362606@qq.com",
+             "password" : "******"
+        }
+        res = self.spider.doSpiderPost(url, args_dict)
+        if "html" in res:
+            flag = True
+        else:
+            flag = False
+        self.assertEqual(flag, True, "do spider tet fail")
+        res = self.spider.doSpiderPost(url)
+        if "html" in res:
+            flag = True
+        else:
+            flag = False
+        self.assertEqual(flag, True, "do spider tet fail")
+
+    def test_doSpiderHeaders(self):
+        """
+        this UT used to test function doSpiderHeaders
+        """
+        url = "passport.csdn.net/account/login"
+        args_dict = {
+            "data":{
+            "userName":"2429362606@qq.com",
+             "password" : "******"},
+            "headers":{ "User-agent":"Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"}
+        }
+        res = self.spider.doSpiderHeaders(url, args_dict)
+        if "html" in res:
+            flag = True
+        else:
+            flag = False
+        self.assertEqual(flag, True, "do spider tet fail")
+
+    def test_doSpiderProxy(self):
+        """
+        this UT used to test function doSpiderProxy
+        """
+        url = "passport.csdn.net/account/login"
+        args_dict = {"headers":{},
+                     "data":{},
+                     "proxySetting":""}
+        res = self.spider.doSpiderProxy(url, args_dict)
         print res
 
+
+
 if __name__ == '__main__':
-    unittest.main()
+    print unittest.main()
